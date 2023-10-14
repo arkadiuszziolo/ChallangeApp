fa0f66181e6e066e852ce4459d043971a9e56992
Arek Zioło <arek_z77@onet.eu>
2023-10-15 00:15:36 +0200

    Update conditions Homework day #4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -6,11 +6,19 @@ if (age < 30 && gender == 'k')
 {
     Console.WriteLine("Kobieta poniżej 30lat");
 }
+else if (!(age > 32) && gender == 'k' && name == "Ewa")
+{
+    Console.WriteLine("Ewa nie ma powyżej 32 lat");
+}
 else if (age == 33 && name == "Ewa")
 {
     Console.WriteLine("Ewa, lat 33");
 }
-else
+else if (gender == 'm' && age < 18)
 {
     Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("Żaden warunek nie został spełniony");
 }
